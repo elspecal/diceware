fd933ecde978edde2c68c8be39074e5e0147a5f1
Miklós Csepella <csepella@tutanota.com>
2021-06-18 12:46:23 +0200

    Preview (#1)
    * Disable config.
    
    * Use next preact plugin.
    Fix issue of initially empty textarea by replacing value attr with content.
    
    * Insert console.logs to debug productin code.
    
    * Remove console.logs.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,3 @@
-module.exports = {
-  webpack: (config, { dev, isServer }) => {
-    //Replace React with Preact
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
-    }
-    return config;
-  },
-};
+const withPreact = require('next-plugin-preact');
+
+module.exports = withPreact({});
